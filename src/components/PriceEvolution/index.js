394857ac5dfafb1a0d2c2c174fbf4e665f9861f7
leonardo <leodanlors1993@gmail.com>
2022-01-11 15:27:48 -0600

    price evolution data

--- src/components/PriceEvolution/index.js
+++ src/components/PriceEvolution/index.js
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Chart from "react-apexcharts";
+import useFetch from './utils/useFetch';
+const priceUrl = 'https://atlantia-dev-test.herokuapp.com/api/price-evolution-chart/'
+const PriceEvolution = () => {
+    const [ options, setOptions] = useState({
+        chart: {
+          id: "price-evolution-bar",
+          toolbar:{show:false}
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+        }
+      });
+      const [series, setSeries] = useState([{
+        name: "series-1",
+        data: [50, 20, 35, 44, 99, 60, 70, 91]
+      },{
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }])
 
-const PriceEvolution = (props) => {
+    const [chartData,loading] =  useFetch( priceUrl)
+
+    /*  
+    {
+        "sku": "039430430493093",
+        "name": "Cerveza XX Ambar 325 ml",
+        "price": 22,
+        "dateExtraction": "10/04/21"
+    }
+    */
     return (
-        <>
-         <div>
-             <p>Price Evolution</p>
-         </div>
-        </>
-    )
+        <div className="app">
+          <div className="row">
+            <div className="mixed-chart">
+              <Chart
+                options={options}
+                series={series}
+                width="100%"
+              />
+            </div>
+          </div>
+        </div>
+      );
 }
 
 export default PriceEvolution;
